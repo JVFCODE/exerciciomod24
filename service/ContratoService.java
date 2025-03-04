ac85fd6902c494cb2b031485c09092f050a32f6a
JVFCODE <164587661+JVFCODE@users.noreply.github.com>
2025-03-04 19:44:51 -0300

    Update ContratoService.java

--- service/ContratoService.java
+++ service/ContratoService.java
@@ -18,4 +18,33 @@ public class ContratoService implements IContratoService {
         contratoDao.salvar();
         return "Sucesso";
     }
+
+    @Override
+    public String buscar(int id) {
+        return contratoDao.buscar(id);
+    }
+
+    @Override
+    public String excluir(int id) {
+        contratoDao.excluir(id);
+        return "Contrato excluído com sucesso";
+    }
+
+    @Override
+    public String atualizar(int id, String novoValor) {
+        contratoDao.atualizar(id, novoValor);
+        return "Contrato atualizado com sucesso";
+    }
+}
+
+package br.com.rpires.service;
+
+/**
+ * @author rodrigo.pires
+ */
+public interface IContratoService {
+    String salvar();
+    String buscar(int id);
+    String excluir(int id);
+    String atualizar(int id, String novoValor);
 }
